5bb4eb90778ddf1ea7ef1ae4032785b6b797faf9
unknown <kumbykays@gmail.com>
2021-08-28 12:05:18 -0500

    fifth commit

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
 function openPage(){
     var x = document.getElementById("search").value;
 
-    if (x==="contact"){
+    if (x==="contact" || x==="Contact" ){
         window.open("./contact.html",("_self"));
     }
 
-    else if (x==="address"){
+    else if (x==="address" || x==="Address"){
         window.open("./contact.html",("_self"));
     }
-    else if (x==="home"){
+    else if (x==="home" || x==="Home"){
         window.open("./index.html",("_self"));  
     }
-    else if (x==="what we do"){
+    else if (x==="What we do"){
         window.open("./about.html",("_self"));   
     }
-    else if (x==="services"){
+    else if (x==="services" || x==="Services"){
         window.open("./about.html",("_self"));   
     }
+    else if (x==="Portifolio" || x==="portifolio"){
+        window.open("./about.html",("_self")); }
     else {
     alert("result not found")    
     }
